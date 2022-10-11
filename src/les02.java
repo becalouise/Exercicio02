efc81db74cd91df47f2bcdf0895981b10d7848b5
becalouise <tsubsarebeca@gmail.com>
2022-10-10 22:34:20 -0300

    Exercício 02

---
+++ src/les02.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class les02 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int numero = sc.nextInt();
+        sc.close();
+        System.out.println("O número digitado foi: " + numero);
+    }
+
+}
